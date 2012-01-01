c62d78f9b8bb254553d8990cb7a5c79cae239bff
Marcel Bruch <marcel.bruch@gmail.com>
2012-01-01 20:02:10 +0100

    fixed build including tests

--- tests/org.eclipselabs.recommenders.tests.codesearchquery.rcp/src/org/eclipselabs/recommenders/test/codesearchquery/Testclass.java
+++ tests/org.eclipselabs.recommenders.tests.codesearchquery.rcp/src/org/eclipselabs/recommenders/test/codesearchquery/Testclass.java
@@ -1,14 +1,23 @@
 package org.eclipselabs.recommenders.test.codesearchquery;
 
 import java.io.IOException;
+
+import org.junit.Test;
+
 public class Testclass extends IOException {
-	public Object someObject = null;
-	public Object theWorldMap = (new Testclass()).someObject;
-	public static void theBestMethodEver() {
-		try {
-		} catch(Exception ex) {
-			Testclass c = new Testclass();
-			Object myObject = c.someObject;
-		}
-	}
+    public Object someObject = null;
+
+    // public Object theWorldMap = (new Testclass()).someObject;
+
+    public static void theBestMethodEver() {
+        try {
+        } catch (final Exception ex) {
+            final Testclass c = new Testclass();
+            final Object myObject = c.someObject;
+        }
+    }
+
+    @Test
+    public void testDummy() {
+    }
 }
