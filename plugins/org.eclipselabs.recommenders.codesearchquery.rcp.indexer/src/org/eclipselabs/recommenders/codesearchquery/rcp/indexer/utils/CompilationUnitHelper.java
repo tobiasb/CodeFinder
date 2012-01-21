e8e00b6e6c56f1e05920d064fcbceb74cedff322
Marcel Bruch <marcel.bruch@gmail.com>
2012-01-21 21:13:08 +0100

    added utility method to parse source from class files if available

--- plugins/org.eclipselabs.recommenders.codesearchquery.rcp.indexer/src/org/eclipselabs/recommenders/codesearchquery/rcp/indexer/utils/CompilationUnitHelper.java
+++ plugins/org.eclipselabs.recommenders.codesearchquery.rcp.indexer/src/org/eclipselabs/recommenders/codesearchquery/rcp/indexer/utils/CompilationUnitHelper.java
@@ -1,5 +1,6 @@
 package org.eclipselabs.recommenders.codesearchquery.rcp.indexer.utils;
 
+import org.eclipse.jdt.core.IClassFile;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
@@ -7,10 +8,18 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 
 public class CompilationUnitHelper {
 
-    public static CompilationUnit parse(ICompilationUnit unit) {
-        ASTParser parser = ASTParser.newParser(AST.JLS3);
+    public static CompilationUnit parse(final ICompilationUnit unit) {
+        ASTParser parser = ASTParser.newParser(AST.JLS4);
         parser.setKind(ASTParser.K_COMPILATION_UNIT);
         parser.setSource(unit);
+        parser.setResolveBindings(true);
+        return (CompilationUnit) parser.createAST(null); // parse
+    }
+
+    public static CompilationUnit parse(final IClassFile type) {
+        ASTParser parser = ASTParser.newParser(AST.JLS4);
+        parser.setKind(ASTParser.K_COMPILATION_UNIT);
+        parser.setSource(type);
         parser.setResolveBindings(true);
         return (CompilationUnit) parser.createAST(null); // parse
     }
