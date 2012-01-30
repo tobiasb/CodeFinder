1c8564c7e7b301d2982d8582ac850d78e1bde72d
Marcel Bruch <marcel.bruch@gmail.com>
2012-01-30 08:41:23 +0100

    [ui] added experimental support for primitive highlighter

--- plugins/org.eclipselabs.recommenders.codesearchquery.rcp/src/org/eclipselabs/recommenders/internal/codesearchquery/rcp/views/SearchQueryView.java
+++ plugins/org.eclipselabs.recommenders.codesearchquery.rcp/src/org/eclipselabs/recommenders/internal/codesearchquery/rcp/views/SearchQueryView.java
@@ -4,9 +4,12 @@ import static java.util.Collections.emptyList;
 import static org.eclipse.jdt.ui.JavaElementLabelProvider.SHOW_OVERLAY_ICONS;
 import static org.eclipse.jdt.ui.JavaElementLabelProvider.SHOW_PARAMETERS;
 import static org.eclipse.jdt.ui.JavaElementLabelProvider.SHOW_POST_QUALIFIED;
+import static org.eclipse.recommenders.utils.Checks.cast;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.CorruptIndexException;
@@ -19,20 +22,32 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.internal.ui.javaeditor.EditorUtility;
+import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
 import org.eclipse.jdt.ui.JavaElementLabelProvider;
 import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.jdt.ui.SharedASTProvider;
+import org.eclipse.jface.text.ITextPresentationListener;
+import org.eclipse.jface.text.TextPresentation;
+import org.eclipse.jface.text.source.SourceViewer;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.recommenders.injection.InjectionService;
+import org.eclipse.recommenders.utils.annotations.Experimental;
 import org.eclipse.recommenders.utils.names.IMethodName;
 import org.eclipse.recommenders.utils.names.ITypeName;
 import org.eclipse.recommenders.utils.names.VmMethodName;
@@ -40,8 +55,11 @@ import org.eclipse.recommenders.utils.names.VmTypeName;
 import org.eclipse.recommenders.utils.rcp.JavaElementResolver;
 import org.eclipse.recommenders.utils.rcp.RCPUtils;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -51,13 +69,18 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.xtext.parser.IParseResult;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.embedded.EmbeddedEditor;
 import org.eclipse.xtext.ui.editor.embedded.EmbeddedEditorFactory;
 import org.eclipse.xtext.ui.editor.embedded.EmbeddedEditorModelAccess;
 import org.eclipse.xtext.ui.editor.embedded.IEditedResourceProvider;
+import org.eclipse.xtext.ui.editor.model.XtextDocument;
+import org.eclipse.xtext.util.concurrent.IUnitOfWork;
 import org.eclipselabs.recommenders.codesearchquery.rcp.Fields;
 import org.eclipselabs.recommenders.codesearchquery.rcp.dsl.LuceneQueryStandaloneSetup;
+import org.eclipselabs.recommenders.codesearchquery.rcp.dsl.luceneQuery.ClauseExpression;
+import org.eclipselabs.recommenders.codesearchquery.rcp.dsl.luceneQuery.Expression;
 import org.eclipselabs.recommenders.codesearchquery.rcp.dsl.ui.contentassist.LuceneQueryProposalProvider;
 import org.eclipselabs.recommenders.codesearchquery.rcp.dsl.ui.contentassist.QueryProposalType;
 import org.eclipselabs.recommenders.codesearchquery.rcp.dsl.ui.internal.LuceneQueryActivator;
@@ -76,6 +99,7 @@ import org.eclipselabs.recommenders.codesearchquery.rcp.termvector.ResourcePathP
 
 import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.google.inject.Injector;
 
 public class SearchQueryView extends ViewPart {
@@ -252,11 +276,97 @@ public class SearchQueryView extends ViewPart {
         searchResultTable.addDoubleClickListener(new IDoubleClickListener() {
 
             @Override
+            @Experimental
             public void doubleClick(final DoubleClickEvent event) {
+
                 final Optional<IJavaElement> first = RCPUtils.first(event.getSelection());
                 if (first.isPresent()) {
                     try {
-                        JavaUI.openInEditor(first.get());
+                        final JavaEditor editor = cast(JavaUI.openInEditor(first.get()));
+                        final SourceViewer s = (SourceViewer) editor.getViewer();
+                        final XtextDocument document = handle.getDocument();
+                        final Set<String> searchTerms = document
+                                .readOnly(new IUnitOfWork<Set<String>, XtextResource>() {
+
+                                    @Override
+                                    public Set<String> exec(final XtextResource state) throws Exception {
+                                        final IParseResult parse = state.getParseResult();
+                                        final EObject rootASTElement = parse.getRootASTElement();
+                                        // TODO Auto-generated method stub
+                                        final Expression luceneQuery = (Expression) rootASTElement;
+                                        final ClauseExpression value2 = luceneQuery.getValue();
+                                        final EObject field = value2.getField();
+                                        final String value = value2.getValue();
+                                        final String _default = value2.getDefault();
+                                        final Set<String> res = Sets.newHashSet();
+                                        if (_default != null) {
+                                            res.add(_default.replaceAll("\\W", "").toLowerCase());
+                                        }
+                                        return res;
+                                    }
+                                });
+
+                        s.addTextPresentationListener(new ITextPresentationListener() {
+
+                            @Override
+                            public void applyTextPresentation(final TextPresentation textPresentation) {
+                                final ICompilationUnit cu = (ICompilationUnit) EditorUtility.getActiveEditorJavaInput();
+                                final Color foreground = JavaUI.getColorManager().getColor(new RGB(255, 0, 0));
+                                final Color background = JavaUI.getColorManager().getColor(new RGB(255, 255, 128));
+                                final Color heuristic = JavaUI.getColorManager().getColor(new RGB(220, 245, 139));
+                                SharedASTProvider.getAST(cu, SharedASTProvider.WAIT_YES, null).accept(new ASTVisitor() {
+                                    @Override
+                                    public boolean visit(final SimpleName node) {
+                                        final String word = node.getIdentifier().toLowerCase();
+                                        for (final String searchterm : searchTerms) {
+                                            word.contains(searchterm);
+                                            setHighlightStyleForNode(textPresentation, foreground, background, node);
+                                        }
+                                        return true;
+                                    }
+
+                                    private void setHighlightStyleForNode(final TextPresentation textPresentation,
+                                            final Color foreground, final Color background, final ASTNode node) {
+                                        final int start = node.getStartPosition();
+                                        final int length = node.getLength();
+                                        calculateRanges(textPresentation, start, length, foreground, background);
+                                    }
+
+                                    @SuppressWarnings("unchecked")
+                                    private void calculateRanges(final TextPresentation textPresentation,
+                                            final int start, final int length, final Color foreground,
+                                            final Color background) {
+                                        final Iterator<StyleRange> srIterator = textPresentation
+                                                .getAllStyleRangeIterator();
+                                        while (srIterator.hasNext()) {
+                                            final StyleRange current = srIterator.next();
+                                            if (current.start > start
+                                                    && current.start + current.length > start + length
+                                                    && current.start < start + length) {
+                                                textPresentation.mergeStyleRange(new StyleRange(current.start, start
+                                                        + length - current.start, foreground, background));
+                                            } else if (current.start >= start
+                                                    && current.start + current.length <= start + length) {
+                                                current.background = background;
+                                                current.foreground = foreground;
+                                            } else if (current.start < start
+                                                    && current.start + current.length > start + length) {
+                                                textPresentation.mergeStyleRange(new StyleRange(start, length,
+                                                        foreground, background));
+                                            } else if (current.start < start
+                                                    && current.length + current.start < start + length
+                                                    && current.start + current.length > start) {
+                                                textPresentation.mergeStyleRange(new StyleRange(start, start + length
+                                                        - current.start + current.length, foreground, background));
+                                            }
+                                        }
+                                    };
+                                });
+
+                            }
+                        });
+                        s.invalidateTextPresentation();
+
                     } catch (final PartInitException e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
